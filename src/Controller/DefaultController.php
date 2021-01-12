f64b8c04b0fdf1b9a6dff695ec1b1692692d99f1
Sidiki <sidikiissadiarra@gmail.com>
2021-01-12 14:41:42 +0000

    first Setup

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,31 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController 
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        
+
+        return $this->render('index.html.twig');
+    }
+
+
+    /**
+     * @Route("/contact", name="con")
+     */
+    public function index(): Response
+    {
+        
+
+        return $this->render('index.html.twig');
+    }
+}
